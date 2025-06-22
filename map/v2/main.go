147f561a518750187b0b42d8aadaceecd950c832
taiyangyi <taiyangyi@163.com>
2025-06-22 18:45:01 +0800

    map说明

--- map/v2/main.go
+++ map/v2/main.go
@@ -2,13 +2,29 @@ package main
 
 import "fmt"
 
+// map的基本操作
 func main() {
 
+	// 插入数据
 	m := make(map[int]string)
 	m[0] = "振铃"
 	m[1] = "摘机"
 	m[2] = "空闲"
 	m[3] = "异常"
+
+	// 获取数据个数
+	fmt.Println(len(m)) // 4
+	m[4] = "其他"
+	fmt.Println(len(m)) // 5
+
+	// 查找和数据的读取
+	_, ok := m[3]
+	if !ok {
+		// key 不在 map中
+		fmt.Println("key=3不在 map中")
+	} else {
+		fmt.Println("key=3在 map中")
+	}
 
 	// 遍历map，首先map遍历是无序的，不论是键还是值，
 	// 如果需要遍历时保持相同的顺序，提要将键做排序处理
